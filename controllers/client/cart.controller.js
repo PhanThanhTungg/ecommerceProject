3a36ba60f2c190430fc35913b2f818bf776af25f
TungPhan03 <tuttutxytin@gmail.com>
2024-08-03 21:44:47 +0700

    create cart page

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,26 @@
 const Cart = require("../../models/cart.model")
+const Product = require("../../models/product.model")
+
+module.exports.indexGET = async (req,res)=>{
+  const cart = await Cart.findOne({_id: req.cookies.cartId})
+
+  let totalPrice = 0
+
+  for(const item of cart.products){
+    const product = await Product.findOne({_id: item.id, status: "active", deleted: false})
+    product.newPrice = (product.price*(100-product.discountPercentage)/100).toFixed(0)
+    
+    item.productInfo = product
+    item.totalPrice = product.newPrice*item.quantity
+    totalPrice += item.totalPrice
+  }
+  
+  cart.totalPrice = totalPrice
+  res.render("client/pages/cart/index.pug",{
+    pageTitle: "Cart",
+    cartDetail: cart
+  })
+}
 
 module.exports.addPOST = async (req,res)=>{
   const cartId = req.cookies.cartId
--- route/client/cart.route.js
+++ route/client/cart.route.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router() //tạo mới 1 router
 const controller = require("../../controllers/client/cart.controller")
 
+router.get("/",controller.indexGET)
+
 router.post("/add/:productId", controller.addPOST)
 
 module.exports = router
