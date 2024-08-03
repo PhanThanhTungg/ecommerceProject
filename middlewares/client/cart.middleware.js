7723d50e9c00860c699ed491373717a806648a61
TungPhan03 <tuttutxytin@gmail.com>
2024-08-03 11:05:04 +0700

    miniCart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -7,6 +7,18 @@ module.exports = async(req,res,next)=>{
     await cart.save()
     const time = 30*24*60*60*1000 //ms
     res.cookie("cartId", cart.id,{expires: new Date(Date.now() + time)})
+  }else{
+    const cart = await Cart.findOne({_id: cartId})
+    if(cart){
+      const totalQuantity = cart.products.reduce((quantity,val)=>quantity+val.quantity,0)
+      cart.totalQuantity = totalQuantity
+      res.locals.miniCart = cart
+    }
+    else{
+      res.clearCookie("cartId")
+      res.redirect("/")
+      return 
+    }
   }
   next()
 }
