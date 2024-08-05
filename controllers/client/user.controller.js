6fd8b403c90fd922d8a198fe90d8b79dd293ff91
TungPhan03 <tuttutxytin@gmail.com>
2024-08-05 10:09:26 +0700

    additional login

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -2,61 +2,83 @@ const md5 = require("md5")
 const User = require("../../models/clientAcc.model")
 
 module.exports.registerGET = async (req,res)=>{
-  res.render("client/pages/user/register", {
-    pageTitle: "Đăng ký tài khoản",
-  })
+  if(!res.locals.user){
+    res.render("client/pages/user/register", {
+      pageTitle: "Đăng ký tài khoản",
+    })
+  }
+  else{
+    res.redirect("/")
+  }
 }
 
 module.exports.registerPOST = async (req,res)=>{
-  const userInfo = {
-    fullName: req.body.fullName,
-    email: req.body.email,
-    password: md5(req.body.password),
+  if(!res.locals.user){
+    const userInfo = {
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: md5(req.body.password),
+    }
+  
+    const user = new User(userInfo)
+    await user.save()
+  
+    res.cookie("tokenUser", user.token);
+  
+    req.flash("success", "Đăng ký thành công")
+    res.redirect("/")
   }
-
-  const user = new User(userInfo)
-  await user.save()
-
-  res.cookie("tokenUser", user.token);
-
-  req.flash("success", "Đăng ký thành công")
-  res.redirect("/")
+  else{
+    res.redirect("/")
+  }
+  
 }
 
 module.exports.loginGET = async (req,res)=>{
-  res.render("client/pages/user/login", {
-    pageTitle: "Đăng nhập",
-  })
+  if(!res.locals.user){
+    res.render("client/pages/user/login", {
+      pageTitle: "Đăng nhập",
+    })
+  }
+  else{
+    res.redirect("/")
+  }
 }
 
 module.exports.loginPOST = async (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
+  if(!res.locals.user){
+    const email = req.body.email
+    const password = req.body.password
 
-  const user = await User.findOne({
-    email: email,
-    deleted: false
-  })
+    const user = await User.findOne({
+      email: email,
+      deleted: false
+    })
 
-  if(!user) {
-    req.flash("error", "Email không tồn tại!")
-    res.redirect("back")
-    return
+    if(!user) {
+      req.flash("error", "Email không tồn tại!")
+      res.redirect("back")
+      return
+    }
+
+    if(md5(password) != user.password) {
+      req.flash("error", "Sai mật khẩu!")
+      res.redirect("back")
+      return
+    }
+
+    if(user.status != "active") {
+      req.flash("error", "Tài khoản đang bị khóa!")
+      res.redirect("back")
+      return
+    }
+
+    res.cookie("tokenUser", user.token)
+    
+    res.redirect("/")
   }
-
-  if(md5(password) != user.password) {
-    req.flash("error", "Sai mật khẩu!")
-    res.redirect("back")
-    return
+  else{
+    res.redirect("/")
   }
-
-  if(user.status != "active") {
-    req.flash("error", "Tài khoản đang bị khóa!")
-    res.redirect("back")
-    return
-  }
-
-  res.cookie("tokenUser", user.token)
   
-  res.redirect("/")
 }
